128bb656d95b64c70ab99fbd82e97aded1947851
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2026-01-02 21:34:12 +0530

    text encoder

---
+++ backend/training/models.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+from transformers import BertModel
+
+
+class TextEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.bert = BertModel.from_pretrained("bert-base-uncased")
+
+        for param in self.bert.parameters():
+            param.requires_grad = False
+
+        self.projection = nn.Linear(768, 128)
+
+    def forward(self, input_ids, attention_mask):
+        # Extract BERT embeddings
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+
+        # Use (CLS) token representation
+        pooled_output = outputs.pooled_output
+
+        return self.projection(pooled_output)
