4dc01e3e318844f7d567a012d81cb5b339cd66d8
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2026-01-03 16:24:50 +0530

    Adding audioencoder

--- backend/training/models.py
+++ backend/training/models.py
@@ -49,4 +49,28 @@ class VideoEncoder(nn.Module):
 class AudioEncoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv_layer = nn.Sequential()
+        self.conv_layer = nn.Sequential(
+            # Lower level feactures
+            nn.Conv1d(64, 64, kernel_size=3),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+            # Higher level feactures
+            nn.Conv1d(64, 128, kernel_size=128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool1d(1),
+        )
+
+        for param in self.conv_layer.parameters():
+            param.requires_grad = False
+
+        self.projection = nn.Sequential(nn.Linear(128, 128), nn.ReLU(), nn.Dropout(0.2))
+
+    def forward(self, x):
+        x = x.squeeze(1)
+
+        features = self.conv_layer(x)
+        # Features output : [batch_size, 128, 1]
+
+        return self.projection(features.squeeze(-1))
