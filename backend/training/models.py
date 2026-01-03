9cae17ce1c9de9cec13c50500e5b77c3924594d1
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2026-01-03 13:10:15 +0530

    fc type error fixed

--- backend/training/models.py
+++ backend/training/models.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from transformers import BertModel
+from torchvision import models as vision_models
 
 
 class TextEncoder(nn.Module):
@@ -16,7 +17,36 @@ class TextEncoder(nn.Module):
         # Extract BERT embeddings
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
 
-        # Use (CLS) token representation
-        pooled_output = outputs.pooled_output
+        # Use [CLS] token representation
+        pooler_output = outputs.pooler_output
 
-        return self.projection(pooled_output)
+        return self.projection(pooler_output)
+
+
+class VideoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.backbone = vision_models.video.r3d_18(pretrained=True)
+
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+
+        num_fts = self.backbone.fc.in_features
+        # self.backbone.fc = nn.Sequential(
+        #     nn.Linear(num_fts, 128), nn.ReLU(), nn.Dropout(0.2)
+        # )
+
+        # for typecheck error
+        head = nn.Sequential(nn.Linear(num_fts, 128), nn.ReLU(), nn.Dropout(0.2))
+        setattr(self.backbone, "fc", head)
+
+    def forward(self, x):
+        # [batch_size, frames, channels, height, width]->[batch_size, channels, frames, height, width]
+        x = x.transpose(1, 2)
+        return self.backbone(x)
+
+
+class AudioEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_layer = nn.Sequential()
