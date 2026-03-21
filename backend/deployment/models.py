57b9726a0667a19279535a3bfd60da0f8e32528b
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2026-03-21 13:27:49 +0530

    adding deployment models

---
+++ backend/deployment/models.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+from transformers import BertModel
+
+
+class TextEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.bert = BertModel.from_pretrained("bert-base-uncased")
+
+        for param in self.bert.parameters():
+            param.requires_grad = False
+
+        self.projection = nn.Linear(768, 128)
